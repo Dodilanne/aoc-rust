9d3eeae5c5c062aafa15a19f0f7a37b7e0f94b6b
Dodilanne <vincaudet@gmail.com>
2023-12-03 20:15:02 -0500

    Cleanup

--- lib/src/solutions/sol_2023_01.rs
+++ lib/src/solutions/sol_2023_01.rs
@@ -1,41 +1,20 @@
 use crate::utils::solver::Solver;
 
 fn solve(input: &str) -> String {
-    let sum: usize = input
+    let sum = input
         .lines()
-        .map(|line| -> usize {
-            let mut f: Option<char> = None;
-            let mut l: Option<char> = None;
+        .map(|line| {
+            let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+            let first = digits.next().expect("no first number");
+            let last = digits.last().unwrap_or(first);
 
-            for c in line.chars() {
-                if c.is_numeric() {
-                    if f.is_some() {
-                        l = Some(c);
-                    } else {
-                        f = Some(c);
-                    }
-                }
-            }
-
-            if l.is_none() {
-                l = f;
-            }
-
-            if let Some(first) = f {
-                if let Some(last) = l {
-                    let res = format!("{}{}", first, last).parse::<usize>();
-                    return match res {
-                        Ok(nbr) => nbr,
-                        _ => 0,
-                    };
-                }
-            }
-
-            return 0;
+            format!("{first}{last}")
+                .parse::<u32>()
+                .expect("invalid number")
         })
-        .sum();
+        .sum::<u32>();
 
-    return sum.to_string();
+    sum.to_string()
 }
 
 pub fn solver() -> anyhow::Result<Solver> {
