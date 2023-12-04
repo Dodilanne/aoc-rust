1e4756c8c558c260fb7144dd7fbab48ec8896158
Dodilanne <vincaudet@gmail.com>
2023-12-03 19:03:38 -0500

    Build generator

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::io::Write;
 
 /// Generate a solution template for a given year and day
 #[derive(Parser, Debug)]
@@ -13,8 +14,72 @@ struct Args {
     day: u8,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    println!("Year {}, Day {}", args.year, args.day);
-    todo!("Implement solution generation");
+
+    generate_solution_file(&args)?;
+    append_to_mod_file(&args)?;
+    update_runner(&args)?;
+
+    Ok(())
+}
+
+fn append_to_mod_file(args: &Args) -> anyhow::Result<()> {
+    let mod_path = format!("lib/src/solutions/mod.rs");
+    let contents = format!(
+        r#"pub mod sol_{}_{:0>2};
+"#,
+        args.year, args.day
+    );
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(mod_path)?;
+    file.write_all(contents.as_bytes())?;
+
+    Ok(())
+}
+
+fn generate_solution_file(args: &Args) -> anyhow::Result<()> {
+    let solution_path = format!("lib/src/solutions/sol_{}_{:0>2}.rs", args.year, args.day);
+    let contents = format!(
+        r#"use crate::utils::solver::Solver;
+
+fn solve(input: &str) -> String {{
+    println!("{{}}", input);
+    String::from(input)
+}}
+
+pub fn solver() -> anyhow::Result<Solver> {{
+    Solver::new({}, {}, solve)
+}}
+"#,
+        args.year, args.day
+    );
+    std::fs::write(solution_path, contents)?;
+
+    Ok(())
+}
+
+fn update_runner(args: &Args) -> anyhow::Result<()> {
+    let path = "runner/src/main.rs";
+    let contents = format!(
+        r#"use std::io::Write;
+
+// Change the year and day to the day you want to run
+use lib::solutions::sol_{}_{:0>2}::solver;
+
+fn main() -> anyhow::Result<()> {{
+    let solution = solver()?.solve();
+    let mut stdout = std::io::stdout();
+    stdout.write_all(solution.as_bytes())?;
+
+    Ok(())
+}}
+"#,
+        args.year, args.day
+    );
+    std::fs::write(path, contents)?;
+
+    Ok(())
 }
